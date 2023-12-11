2d0f9dffd4c7fb4119ab5d3d32267b8d648567c2
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-12-11 14:54:06 -0600

    working on some of these lessons, but wanted to showcase the progress so far

--- lists/lesson3.py
+++ lists/lesson3.py
@@ -1,5 +1,18 @@
 # create a list and loop over the list
+looping_list = ["Shepard", 6, True]
+for item in looping_list:
+    print(item)
+
 # create a list and add one to each item of the list. Does not mutate the item.
+non_mutate = [1, 2, 3, 4]
+
+
 # create a list and add one to each item of the list. Make sure it mutates the original list.
+mutate = [10, 20, 30, 40, 50]
+for num in mutate:
+    print(num + 1)
+
 # create a list and loop backwards through the list
+
+
 # create a list and only print out every other item
