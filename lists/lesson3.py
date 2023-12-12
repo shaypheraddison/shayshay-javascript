c4149475c887f8fb56bdc241cc8c63e9fc553227
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-12-12 11:14:25 -0600

    completed all lessons for lesson 3 ! this was a challenge

--- lists/lesson3.py
+++ lists/lesson3.py
@@ -5,12 +5,24 @@ for item in looping_list:
 
 # create a list and add one to each item of the list. Does not mutate the item.
 non_mutate = [1, 2, 3, 4]
+new_non = non_mutate.copy()
+for x in non_mutate:
+    new_non.append(x + 1)
+print(new_non)
 
 
 # create a list and add one to each item of the list. Make sure it mutates the original list.
 mutate = [10, 20, 30, 40, 50]
-for num in mutate:
-    print(num + 1)
+updated = [num+1 for num in mutate]  #THANK YOU PYTHON DOCUMENTATION FOR LIST COMPREHENSION !!
+print(updated)
+
+#original block I wrote for line 14. end result wasn't what I think was wanted
+# mutate = [10, 20, 30, 40, 50]
+# for num in mutate:
+#     updated = []
+#     num += 1
+#     updated.append(num)
+#     print(updated)
 
 # create a list and loop backwards through the list
 backwards = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
@@ -18,3 +30,6 @@ for i in reversed(backwards):
     print(i)
 
 # create a list and only print out every other item
+even = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+every_other = even[1::2]   #[start:end:step] 
+print(every_other)
