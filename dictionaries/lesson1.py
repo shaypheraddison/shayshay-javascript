23a5e53f84fe85f303bc0d959fe0a3f4317c45b4
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-12-12 15:26:00 -0600

    completed lesson 1.

--- dictionaries/lesson1.py
+++ dictionaries/lesson1.py
@@ -1,7 +1,47 @@
 # create an empty dictionary
+empty = {}
+
 # create a dictionary with keys and values being strings
+str_dictionary = {
+    "first": "high school diploma",
+    "second": "associates degree:",
+    "third": "bachelors degree",
+    "fourth": "masters degree",
+    "fifth": "phd"
+}
+
 # create a dictionary with keys being strings and values being int
+int_dictionary = {
+    "num_1": 10,
+    "num_2": 20,
+    "num_3": 30,
+    "num_4": 40,
+    "num_5": 50
+}
+
 # create a dictionary with keys being string and values being float
+float_dictionary = {
+    "float_1": 0.25,
+    "float_2": 0.50,
+    "float_3": 0.75,
+    "float_4": 1.00
+}
+
 # create a dictionary with keys being string and values being bool
+bool_dictionary = {
+    "yes": True,
+    "no": False
+}
+
 # create a dictionary with keys being string and values being lists
+list_dictionary = {
+    "list_1": [1, 2, 3, 4],
+    "list_2": [5, 6, 7, 8],
+    "list_3": [9, 10, 11, 12]
+}
+
 # create a dictionary with keys being string and values being dictionaries
+dictionary_dictionary = {
+    "dict_1": {"sport": "baseball", "team": "Rangers"},
+    "dict_2": {"sport": "football", "team": "Titans"}
+}
