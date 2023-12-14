8292927ef7b275dce73f0500b26402875f534eae
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-12-14 08:45:12 -0600

    Completed exercise. I like what I did, but I do plan on going back to it and use a dictionary with a loop

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -5,3 +5,17 @@
 #    *****
 #   *******
 # no copying this ascii image and printing it out
+one = "*"
+new_one = one.center(10)
+three = "*" * 3
+new_three = three.center(9)
+five = "*" * 5
+new_five = five.center(10)
+seven = "*" * 7
+new_seven = seven.center(9)
+print(new_one)
+print(new_three)
+print(new_five)
+print(new_seven)
+
+# Thank you Python documentation for .center(). Looks ugly now but it sure does work ! 
