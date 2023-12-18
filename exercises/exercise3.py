562217907f591fcc2b032cc6b3215d6345768d33
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-12-18 15:16:38 -0600

    updated exercise 3

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,4 +1,10 @@
-# # create a deck of cards where the cards are a dictionary
+# create a deck of cards where the cards are a dictionary
+values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+for value in values:
+    for suit in suits:
+        card = {value : suit}
+        print(card)
 
 # card = {
 #     "value": "King",
@@ -26,29 +32,29 @@
 # print(card_4)
 #bring same value to the same dictionary
 
-values = {
-    0: "Ace",
-    1: "2",
-    2: "3",
-    3: "4",
-    4: "5",
-    5: "6",
-    6: "7",
-    7: "8",
-    8: "9",
-    9: "10",
-    10: "Jack",
-    11: "Queen",
-    12: "King"
-}
+# values = {
+#     0: "Ace",
+#     1: "2",
+#     2: "3",
+#     3: "4",
+#     4: "5",
+#     5: "6",
+#     6: "7",
+#     7: "8",
+#     8: "9",
+#     9: "10",
+#     10: "Jack",
+#     11: "Queen",
+#     12: "King"
+# }
 
-suits = {
-    0: "Clubs",
-    1: "Hearts",
-    2: "Diamonds",
-    3: "Spades"
-}
+# suits = {
+#     0: "Clubs",
+#     1: "Hearts",
+#     2: "Diamonds",
+#     3: "Spades"
+# }
 
-for value in values.values():
-    for suit in suits.values():
-        print(f"{value} of {suit}")
+# for value in values.values():
+#     for suit in suits.values():
+#         print(f"{value} of {suit}")
