c0bb5b2475523d18da3d90a0a4cc15baf8cdbf57
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2023-12-26 13:57:32 -0600

    Completed and tested with multiple equations

--- exercises/exercise8.py
+++ exercises/exercise8.py
@@ -12,27 +12,43 @@ single_number = question.split()
 
 def multiply():
     mult_index = single_number.index("*")
-    num_1 = int(single_number[mult_index - 1])
-    num_2 = int(single_number[mult_index + 1])
-    return num_1 * num_2
+    num_1 = single_number[mult_index - 1]
+    num_2 = single_number[mult_index + 1]
+    if ' ' in single_number:
+        single_number.remove(' ')
+    else:
+        pass
+    return float(num_1) * float(num_2)
 
 def divide():
     div_index = single_number.index("/")
-    num_1 = float(single_number[div_index - 1])
-    num_2 = float(single_number[div_index + 1])
-    return num_1 / num_2
+    num_1 = single_number[div_index - 1]
+    num_2 = single_number[div_index + 1]
+    if ' ' in single_number:
+        single_number.remove(' ')
+    else:
+        pass
+    return float(num_1) / float(num_2)
 
 def addition():
     add_index = single_number.index("+")
-    num_1 = int(single_number[add_index - 1])
-    num_2 = int(single_number[add_index + 1])
-    return num_1 + num_2
+    num_1 = single_number[add_index - 1]
+    num_2 = single_number[add_index + 1]
+    if ' ' in single_number:
+        single_number.remove(' ')
+    else:
+        pass
+    return float(num_1) + float(num_2)
 
 def subtraction():
     sub_index = single_number.index("-")
-    num_1 = int(single_number[sub_index - 1])
-    num_2 = int(single_number[sub_index + 1])
-    return num_1 - num_2
+    num_1 = single_number[sub_index - 1]
+    num_2 = single_number[sub_index + 1]
+    if ' ' in single_number:
+        single_number.remove(' ')
+    else:
+        pass
+    return float(num_1) - float(num_2)
 
 def check_div_mult_order():
     if single_number.index("/") < single_number.index("*"):
@@ -46,55 +62,96 @@ def check_add_sub_order():
     else:
        return addition()
     
-def replace_div():
+def div_num_index():
     div_index = single_number.index("/")
-    left_of_div = single_number.index(div_index - 1)
-    right_of_div = single_number.index(div_index + 1)
-    div_no_more = single_number[left_of_div:div_index:right_of_div] = " "
-    return div_no_more
+    left_of_div = single_number[div_index - 1]
+    left_div_index = single_number.index(left_of_div)
+    right_of_div = single_number[div_index + 1]
+    right_div_index = single_number.index(right_of_div)
+    single_number[left_div_index:right_div_index] = " "
+    return single_number
+    # return div_no_more
 
-def replace_mult():
+def mult_num_index():
     mult_index = single_number.index("*")
-    left_of_mult = single_number.index(mult_index - 1)
-    right_of_mult = single_number.index(mult_index + 1)
-    mult_no_more = single_number[left_of_mult:mult_index:right_of_mult] = " "
-    return mult_no_more
+    left_of_mult = single_number[mult_index - 1]
+    left_mult_index = single_number.index(left_of_mult)
+    right_of_mult = single_number[mult_index + 1]
+    right_mult_index = single_number.index(right_of_mult)
+    single_number[left_mult_index:right_mult_index] = " "
+    return single_number
+    # return mult_no_more
 
-def replace_sub():
+def sub_num_index():
     sub_index = single_number.index("-")
-    left_of_sub = single_number.index(sub_index - 1)
-    right_of_sub = single_number.index(sub_index + 1)
-    sub_no_more = single_number[left_of_sub:sub_index:right_of_sub] = " "
-    return sub_no_more
+    left_of_sub = single_number[sub_index - 1]
+    left_sub_index = single_number.index(left_of_sub)
+    right_of_sub = single_number[sub_index + 1]
+    right_sub_index = single_number.index(right_of_sub)
+    single_number[left_sub_index:right_sub_index] = " "
+    return single_number
+    # return sub_no_more
 
-def replace_add():
+def add_num_index():
     add_index = single_number.index("+")
-    left_of_add = single_number.index(add_index - 1)
-    right_of_add = single_number.index(add_index + 1)
-    add_no_more = single_number[left_of_add:add_index:right_of_add] = " "
-    return add_no_more
+    left_of_add = single_number[add_index - 1]
+    left_add_index = single_number.index(left_of_add)
+    right_of_add = single_number[add_index + 1]
+    right_add_index = single_number.index(right_of_add) 
+    single_number[left_add_index:right_add_index] = " "
+    return single_number
+    # return add_no_more
 
+def answer_is():
+    if ' ' in single_number:
+        single_number.remove(' ')
+    else:
+        pass
+    answer = float(single_number[0])
+    return answer
 
 for ops in single_number:
+    if "*" not in single_number:
+        continue
     if "*" in single_number:
+        if "/" not in single_number:
+            continue
         if single_number.index("/") < single_number.index("*"):
-            b = check_div_mult_order()
-            replace_div()
-        a = multiply()
-        replace_mult()
+            b = float(check_div_mult_order())
+            div_index = single_number.index("/")
+            div_num_index()
+            single_number[div_index] = b
+        a = float(multiply())
+        mult_index = single_number.index("*")
+        mult_num_index()
+        single_number[mult_index] = a
     elif "/" in single_number:
-        b = divide()
-        replace_div()
+        b = float(divide())
+        div_index = single_number.index("/")
+        div_num_index()
+        single_number[div_index] = b
+    if "+" not in single_number:
+        continue
     elif "+" in single_number:
+        if "-" not in single_number:
+                continue
         if single_number.index("-") < single_number.index("+"):
             d = check_add_sub_order()
-            replace_sub()
+            sub_index = single_number.index("-")
+            sub_num_index()
+            single_number[sub_index] = d
         c = addition()
-        replace_add()
+        add_index = single_number.index("+")
+        add_num_index()
+        single_number[add_index] = c
     elif "-" in single_number:
+        if "-" not in single_number:
+                pass
         d = subtraction()
-        replace_sub()
+        sub_index = single_number.index("-")
+        sub_num_index()
+        single_number[sub_index] = d
 
 
-print(a + b + c + d)
+print(f"The answer to your equation is: {answer_is():.3f}")
 print(5 + 10 - 2 / 15 * 20 + 99999)
