b83f5286ad7220e330cfc7ba1a6aa4f4a5d36ea1
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-25 18:08:58 -0500

    completed types lesson 1

--- types/lesson1.js
+++ types/lesson1.js
@@ -1,14 +1,23 @@
 // # create a variable of each type
 // # int
+const firstNum = 44;
 
 // # string
+const firstString = "Good day to you.";
 
 // # bool
+const firstBool = false;
 
 // # float
+const fistFloat = 908.1239;
 
-// # list
+// # array
+const firstArray = ["JavaScript", "Python", "HTML", "CSS"];
 
-// # dict
+// # object
+const firstObject = {
+    "testKey": "Test Value !"
+}
 
 // # tuple
+const firstTuple = ("Ohtani", "is", "innocent")
