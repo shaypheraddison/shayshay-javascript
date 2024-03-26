15aca08ebc661f197bf09a71381350a1cf059d98
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-25 22:03:16 -0500

    added git ignore file and completed first lesson

--- types/lesson1.js
+++ types/lesson1.js
@@ -1,14 +1,23 @@
 // # create a variable of each type
 // # int
+const testInt = 10;
 
 // # string
+const testString = "Good day to you.";
 
 // # bool
+const testBool = true;
 
 // # float
+const testFloat = 9.99999;
 
-// # list
+// # array
+const testArray = ["JavaScript", "Python", "HTML", "CSS"];
 
-// # dict
+// # object
+const testObject = {
+    "testKey": "testValue"
+};
 
 // # tuple
+const testTuple = (2024, "World", "Series", "Champs")
