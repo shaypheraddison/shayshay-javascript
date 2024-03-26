e7f4d41a69a515be9dbdf774ad3b99c388aab449
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-26 14:19:13 -0500

    correct terminology for files

---
+++ arrays/lesson2.js
@@ -0,0 +1,31 @@
+// # add an item to a array
+const addArray = [];
+addArray.fill("adding", "this", "text");
+console.log(addArray);
+
+// # remove an item from a array
+
+
+// # add two arrays together
+const array1 = ["test1", "test-1", "test 1"];
+const array2 = ["test2", "test-2", "test 2"];
+const newArray = array1.concat(array2);
+console.log(newArray);
+
+// # get the difference between two arrays(removing same unique values in both arrays)
+
+
+// # insert an item at a given position using a array
+
+
+// # remove an item at a given position using a array
+
+
+// # remove all items from a array
+
+
+// # sort a array
+
+
+// # reverse elements in a array
+
---
+++ arrays/lesson3.js
@@ -0,0 +1,9 @@
+// # create a array and loop over the array
+
+// # create a array and add one to each item of the array. Does not mutate the item.
+
+// # create a array and add one to each item of the array. Make sure it mutates the original array.
+
+// # create a array and loop backwards through the array
+
+// # create a array and only print out every other item
