38decf2a9682273c91ab0c7cf3da7764ac7dcbf2
Shaypher Ashe <149912769+shaypheraddison@users.noreply.github.com>
2024-03-26 21:08:49 -0500

    completed lesson 1

--- lists/lesson1.js
+++ lists/lesson1.js
@@ -1,11 +1,17 @@
-// # create an empty lists
+// # create an empty array
+const myArray = [];
 
-// # create a list with strings
+// # create an array with strings
+const stringArray = ["string 1", "string 2", "string 3"];
 
-// # create a list with ints
+// # create an array with ints
+const intArray = [10, 11, 12, 13];
 
-// # create a list with bool
+// # create an array with bool
+const boolArray = [true, false, false];
 
-// # create a list with floats
+// # create an array with floats
+const floatArray = [10.1, 10.5, 11.1, 11.5];
 
-// # create a list within a list
+// # create an array within a array
+const nestArray = [[1, 2, 3, 4] ["nest 1", "nest 2", "nest 3"]];
