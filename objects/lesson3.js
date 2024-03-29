69ab5534b909ad59561cd4528436de0c6ab62515
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-29 12:34:08 -0500

    complete lesson 3

--- objects/lesson3.js
+++ objects/lesson3.js
@@ -1,3 +1,24 @@
 // # create a dictionary and loop over each item in the dictionary
+const loopingDict = {
+    infield: ["pitcher", "catcher", "first base", "second base", "shortstop", "third base"],
+    outfield: ["left field", "center field", "right field"],
+    pitchers: ["starters", "relievers", "closer"]
+};
+for (position in loopingDict) {
+    console.log(position);
+    console.log(loopingDict[position]);
+}
 
 // # create a dictionary and loop over each item while mutating the value for each key.
+const mutate = {
+    extraPoint: 1,
+    safety: 2,
+    fieldGoal: 3,
+    touchdown: 6
+};
+
+for (point in mutate) {
+    console.log(point);
+    mutate[point] += 1;
+    console.log(mutate[point]);
+}
