03ca78b12785d18ab1625d972c2bba83dbf6a054
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-29 11:57:10 -0500

    completed lesson 2

--- objects/lesson2.js
+++ objects/lesson2.js
@@ -1,10 +1,36 @@
-// # create a dictionary and add a value to it
+// # create a object and add a value to it
+const object1 = {};
+object1.car = "toyota";
+object1.color = "white";
+object1.miles = 28000;
+console.log(object1);
 
+// # create a object and access a key/value from it
+const object2 = {
+    name: "Gary",
+    job: "Garbaage Man",
+    years: 15,
+    pay: 65000
+};
+console.log(object2.name)
+console.log(object2.job)
 
-// # create a dictionary and access a key/value from it
+// # create a object and delete a value from it
+const object3 = {
+    switches: "brown",
+    ledLights: true,
+    color: "navy",
+    wired: false
+};
+delete object3["ledLights"];
+console.log(object3);
 
-
-// # create a dictionary and delete a value from it
-
-
-// # create a dictionary and change a value from it
+// # create a object and change a value from it
+const object4 = {
+    make: "Toyota",
+    model: "Camry",
+    color: "black",
+    driveType: "automatic"
+};
+object4["color"] = "silver";
+console.log(object4);
