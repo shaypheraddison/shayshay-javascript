7194a52383e5bdccb1a923cb35881c56bdc6c281
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-29 16:12:08 -0500

    completed exercises 1 and 2

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -9,3 +9,12 @@
 // # print(king_of_hearts)
 // # print(king_of_spades)
 
+const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+const faces = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+console.log(suits);
+console.log(faces);
+for (let suit of suits) {
+    for (let face of faces) {
+        console.log(face + " of " + suit);
+    }
+}
