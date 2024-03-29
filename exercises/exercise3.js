a982a77108eba3818f5e83fd4412f8069d45f438
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-29 16:27:38 -0500

    completed exercises 3 and 6

--- exercises/exercise3.js
+++ exercises/exercise3.js
@@ -1,2 +1,14 @@
 // # create a deck of cards where the cards are a dictionary
 
+const cards = {
+    faces: ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"],
+    suits: ["Hearts", "Clubs", "Diamonds", "Spades"]
+};
+
+for (card in cards) {
+    for (let face of cards.suits) {
+        for (let suit of cards.faces) {
+            console.log(suit + " of " + face);
+        }
+    }
+}
