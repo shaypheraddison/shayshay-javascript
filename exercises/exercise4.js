6928148d91e35185f32e96e4aecdfc53a8350bf3
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-30 16:25:14 -0500

    completed lesson 4

--- exercises/exercise4.js
+++ exercises/exercise4.js
@@ -5,4 +5,14 @@
 // #    *****
 // #   *******
 
+const stars = ["*", "***", "*****", "*******"];
 
+const star1 = "    *";
+const star2 = "   ***";
+const star3 = "  *****";
+const star4 = " *******";
+
+console.log(star1);
+console.log(star2);
+console.log(star3);
+console.log(star4);
