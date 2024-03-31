f111575e6d10b846a7c65fe2e8fbc5c2faa95245
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-03-31 00:09:59 -0500

    completed exercise 5

--- exercises/exercise5.js
+++ exercises/exercise5.js
@@ -4,3 +4,25 @@
 // # the inputs are item and birth year
 
 
+const item = prompt("Would you like tobacco or alcohol?").toLowerCase();
+const age = prompt("How old are you?");
+
+if (item == "tobacco") {
+    if (age >= 18) {
+        alert("Be safe with your tobacco please.");
+    } else if (age < 18) {
+        alert("You need to be older to get tobacco.");
+    } else {
+        alert("Please enter a proper number for your age.");
+    }
+} else if (item == "alcohol") {
+    if (age >= 21) {
+        alert("Be safe with your alcohol please.");
+    } else if (age < 21) {
+        alert("You need to be older to get alcohol.");
+    } else {
+        alert("Please enter a proper number for your age.");
+    }
+} else {
+    alert("Please choose between tobacco or alcohol.");
+}
