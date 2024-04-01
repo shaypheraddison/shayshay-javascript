2b93359add5560783ce5c1b059eef96467cfebc6
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-04-01 15:27:35 -0500

    set the result to an object. added console print to verify functionality of the program

--- exercises/exercise3.js
+++ exercises/exercise3.js
@@ -1,14 +1,12 @@
 // # create a deck of cards where the cards are a dictionary
 
-const cards = {
-    faces: ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"],
-    suits: ["Hearts", "Clubs", "Diamonds", "Spades"]
-};
 
-for (card in cards) {
-    for (let face of cards.suits) {
-        for (let suit of cards.faces) {
-            console.log(suit + " of " + face);
-        }
+const faces = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+
+for (let face of faces) {
+    for (let suit of suits) {
+        newDeck = {Face: face, Suit: suit};
+        console.log(newDeck);
     }
 }
