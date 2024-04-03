0b8e8afb34adcaeb20fc2d043c90768458026654
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-04-02 23:18:49 -0500

    completed exercise 1. Also added dom.html for referencing things throughout the exercises

--- dom/exercise1.js
+++ dom/exercise1.js
@@ -11,3 +11,22 @@
     - class name
     - id name
 */
+
+const singleP = document.querySelector("p");
+console.log(singleP);
+
+const singleClass = document.querySelector("h1.headers");
+console.log(singleClass);
+
+const singleId = document.querySelector("#ul1")
+console.log(singleId);
+
+const allP = document.querySelectorAll("p");
+console.log(allP);
+
+const allClass = document.querySelectorAll("ul.listyBoyz");
+console.log(allClass);
+
+const allId = document.querySelectorAll("#ul2")
+console.log(allId);
+
