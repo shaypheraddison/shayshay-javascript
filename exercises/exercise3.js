0c32e1ca05a94c91b30ebdda463838721cfea7dd
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-04-02 22:26:53 -0500

    completed exercise 3 and created an array that consist of cards as objects

--- exercises/exercise3.js
+++ exercises/exercise3.js
@@ -4,9 +4,9 @@
 const faces = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
 const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 
+var newDeck = [];
 for (let face of faces) {
     for (let suit of suits) {
-        newDeck = {Face: face, Suit: suit};
-        console.log(newDeck);
+        newDeck.push({Face: face, Suit: suit});
     }
-}
+} console.log(newDeck);
