fb2722c0e566afc5ae00dc59df51524518253f52
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-04-03 15:28:04 -0500

    completed exercise 3

--- dom/exercise3.js
+++ dom/exercise3.js
@@ -1,2 +1,15 @@
 // make all elements with a certain class turn to the color pink
+
+const colorAll = document.querySelectorAll(".mainP");
+
+for (let item = 0; item < colorAll.length; item++) {
+    colorAll[item].setAttribute("style", "color: pink");
+}
+
 // make all elements with a certain class font larger
+
+const bigAll = document.querySelectorAll(".headers");
+
+for (let h = 0; h < bigAll.length; h++) {
+    bigAll[h].setAttribute("style", "font-size: 100px");
+}
