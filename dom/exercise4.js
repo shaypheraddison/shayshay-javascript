b51b5f8c19da9e061048800be07ad4dfecc83162
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-04-04 13:42:28 -0500

    completed exercise 4. added elements in dom.html for reference in exercise 4

--- dom/exercise4.js
+++ dom/exercise4.js
@@ -1,4 +1,33 @@
 // add the ability to when a user clicks a button the word "click" appears in the console
+const clickMe = document.querySelector("#clickButton");
+
+clickMe.addEventListener("click", function() {
+    console.log("Button has been clicked !");
+});
+
 // add the ability to when a user clicks a button an alert happens on the page
+const alertMe = document.querySelector("#alertButton");
+
+alertMe.addEventListener("click", function() {
+    alert("ALERT ALERT ALERT !!! ");
+});
+
 // add the ability to when a user clicks a button a paragraph element is deleted
+const deleteMe = document.querySelector("#removeButton");
+
+deleteMe.addEventListener("click", function() {
+    const deleteP = document.querySelector("#fourthP");
+    deleteP.setAttribute("style", "display: none");
+});
+
+
 // add the ability to when a user clicks a button a paragraph element is added
+const addMe = document.querySelector("#addButton");
+
+addMe.addEventListener("click", function() {
+    const addP = document.createElement("p");
+    const addText = document.createTextNode("This is a new paragraph made from a button !");
+    addP.appendChild(addText);
+    const lowerList = document.querySelector("#listH");
+    document.body.insertBefore(addP, lowerList);
+})
