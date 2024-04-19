1628714d8ae6dbd4435ef052c1caea1d171f7683
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-04-18 23:17:38 -0500

    calculator is functioning with clicking of the buttons. math is done properly. added a clear button to clear display as well

--- dom/exercise6-calculator.js
+++ dom/exercise6-calculator.js
@@ -7,30 +7,67 @@
 // # make sure to handle any data type such as int and floats
 // # make sure to apply the correct mathmetical sequence to process the equation
 
+let displayBox = document.querySelector("#display-box");
 
-const calcDisplay = document.querySelector("#display-box");
+function displayValue(x) {
+    displayBox.textContent += x;
+    return x;
+}
 
-const numberButtons = document.querySelectorAll(".number");
-const mathButtons = document.querySelectorAll(".math-button");
-console.log(numberButtons);
-console.log(mathButtons);
+function clearDisplay() {
+    displayBox.textContent = "";
+}
 
 
-function displayValue(value) {
-    calcDisplay.textContent = value;
-};
-
-function clickNumberButtons() {
-    numberButtons.forEach(function (button) {
-        button.addEventListener("click", function() {
-            const buttonValue = button.textContent;
-            displayValue(buttonValue);
-        });
-    });
-};
-
-clickNumberButtons()
+function doTheMath() {
+    let equation = displayBox.textContent;
+    let finalize = eval(equation);
+    document.querySelector("#display-box").textContent = finalize;
+    return finalize;
+}
 
 
 
 
+
+
+
+// const calcDisplay = document.querySelector("#display-box");
+// const numberButtons = document.querySelectorAll(".number");
+// const mathButtons = document.querySelectorAll(".math-button");
+
+// // display value on display box
+// function displayValue(value) {
+//     calcDisplay.textContent = value;
+// };
+
+// // make a function that changes the number displayed
+// function clickNumberButtons() {
+//     numberButtons.forEach(function(button) {
+//         button.addEventListener("click", function() {
+//             let buttonValue = button.textContent;
+//             displayValue(buttonValue);
+//         });
+//     });
+// };
+// clickNumberButtons();
+
+// // make a function the adds the operator to the display
+// function clickOperatorButtons() {
+//     mathButtons.forEach(function(button) {
+//         button.addEventListener("click", function () {
+//             let operatorButton = button.textContent;
+//             displayValue(operatorButton);
+//         });
+//     });
+// };
+// clickOperatorButtons();
+
+// // make something that stores the clicked data
+// let displayData = "";
+// numberButtons.forEach(function(button) {
+//     button.addEventListener("click", function () {
+//         displayData += clickNumberButtons();
+//         return displayData;
+//     });
+// });
