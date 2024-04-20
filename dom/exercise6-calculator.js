41c701b24104929ab599a288f49fa4b62c8fb9b6
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-04-20 16:07:46 -0500

    completed keystroke event for calculator

--- dom/exercise6-calculator.js
+++ dom/exercise6-calculator.js
@@ -7,18 +7,22 @@
 // # make sure to handle any data type such as int and floats
 // # make sure to apply the correct mathmetical sequence to process the equation
 
+// get text into the display box
 let displayBox = document.querySelector("#display-box");
 
 function displayValue(x) {
-    displayBox.textContent += x;
-    return x;
+    if (x !== "Enter") {
+        displayBox.textContent += x;
+        return x;
+    };
 }
 
+// clear display when needed
 function clearDisplay() {
     displayBox.textContent = "";
 }
 
-
+// evaluate the text in the display box and complete the equation
 function doTheMath() {
     let equation = displayBox.textContent;
     let finalize = eval(equation);
@@ -26,48 +30,39 @@ function doTheMath() {
     return finalize;
 }
 
+// make an object storing the buttons to reference with keystrokes
+const allowedKeys = {
+    '0': document.querySelector("#button0"),
+    '1': document.querySelector("#button1"),
+    '2': document.querySelector("#button2"),
+    '3': document.querySelector("#button3"),
+    '4': document.querySelector("#button4"),
+    '5': document.querySelector("#button5"),
+    '6': document.querySelector("#button6"),
+    '7': document.querySelector("#button7"),
+    '8': document.querySelector("#button8"),
+    '9': document.querySelector("#button9"),
+    '+': document.querySelector("#button-add"),
+    '-': document.querySelector("#button-minus"),
+    '*': document.querySelector("#button-multiply"),
+    '/': document.querySelector("#button-divide"),
+    '.': document.querySelector("#button-decimal"),
+    'Enter': document.querySelector("#button-result"),
+    'Backspace': document.querySelector("#clear-button")
+};
 
-
-
-
-
-
-// const calcDisplay = document.querySelector("#display-box");
-// const numberButtons = document.querySelectorAll(".number");
-// const mathButtons = document.querySelectorAll(".math-button");
-
-// // display value on display box
-// function displayValue(value) {
-//     calcDisplay.textContent = value;
-// };
-
-// // make a function that changes the number displayed
-// function clickNumberButtons() {
-//     numberButtons.forEach(function(button) {
-//         button.addEventListener("click", function() {
-//             let buttonValue = button.textContent;
-//             displayValue(buttonValue);
-//         });
-//     });
-// };
-// clickNumberButtons();
-
-// // make a function the adds the operator to the display
-// function clickOperatorButtons() {
-//     mathButtons.forEach(function(button) {
-//         button.addEventListener("click", function () {
-//             let operatorButton = button.textContent;
-//             displayValue(operatorButton);
-//         });
-//     });
-// };
-// clickOperatorButtons();
-
-// // make something that stores the clicked data
-// let displayData = "";
-// numberButtons.forEach(function(button) {
-//     button.addEventListener("click", function () {
-//         displayData += clickNumberButtons();
-//         return displayData;
-//     });
-// });
+// make a function for pressing the key down
+function keyboardStroke() {
+    document.addEventListener("keydown", function(event) {
+        const keyPressed = event.key;
+        if (allowedKeys[keyPressed]) {
+            displayValue(keyPressed);
+            if (event.key === "Enter") {
+                doTheMath();
+            } else if (event.key === "Backspace") {
+                clearDisplay();
+            };
+        };
+    });
+};
+keyboardStroke();
