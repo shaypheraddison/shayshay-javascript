aecd4b4a8a178c1d08b00e7be2d00e4d947bab72
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-05-08 23:35:29 -0500

    added gifs for background to reflect current weather later on

--- weather-app/weather-app.js
+++ weather-app/weather-app.js
@@ -1,14 +1,24 @@
 // Take data from the api and display it to the specified locations on the webpage
 // First thing to do is to 'fetch' the data from any weather api
 
-// const url = fetch("https://api.weather.gov/");
-
-
-// url.then(response => response.json);
-
-// console.log(url)
-
 //create a function that will change the images at a given grid spot
+
+const sunnyImage = new Image();
+sunnyImage.src = "Resources/sunny.png";
+
+const cloudyImage = new Image();
+cloudyImage.src = "Resources/cloudy.png";
+
+const windyImage = new Image();
+windyImage.src = "Resources/windy.png";
+
+const rainyImage = new Image();
+rainyImage.src = "Resources/rainy.png";
+
+const snowyImage = new Image();
+snowyImage.src = "Resources/snowy.png";
+
+const imageArray = [sunnyImage, cloudyImage, windyImage, rainyImage, snowyImage];
 
 const firstImage = document.getElementById("image1");
 const secondImage = document.getElementById("image2");
@@ -21,23 +31,3 @@ const gridImages = {
     thirdDay: thirdImage,
     fourthDay: fourthImage
 };
-
-const sunnyImage = new Image();
-sunnyImage.src = "Resources/sunny.png"
-
-const cloudyImage = new Image();
-cloudyImage.src = "Resources/cloudy.png"
-
-const windyImage = new Image();
-windyImage.src = "Resources/windy.png"
-
-const rainyImage = new Image();
-rainyImage.src = "Resources/rainy.png"
-
-const snowyImage = new Image();
-snowyImage.src = "Resources/snowy.png"
-
-
-function imageSwap() {
-    
-}
