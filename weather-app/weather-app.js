4785dcd85f8e4e9240d4b4f78a34fcb0a9b9302b
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-05-10 23:49:54 -0500

    added functions to obtain lattitude and longitude based on zip code entered in the text box

--- weather-app/weather-app.js
+++ weather-app/weather-app.js
@@ -3,41 +3,85 @@
 
 //create a function that will change the images at a given grid spot
 
-let inputBox = document.getElementById("searchLocation");
+const locationInput = document.getElementById("searchLocation");
+const weatherBox = document.querySelector(".weatherDisplayBox");
+const inputBox = document.getElementById("locationBox");
+const searchButton = document.getElementById("searchButton");
+const apiKey = "62631cea2487c68825327dafba54dd27";
 
-const sunnyImage = new Image();
-sunnyImage.src = "Resources/sunny.png";
+searchButton.addEventListener("click", event => {
+    const location = locationInput.value;
+    getZipLat(location);
+    getZipLon(location);
+    getCityData();
+});
 
-const cloudyImage = new Image();
-cloudyImage.src = "Resources/cloudy.png";
 
-const windyImage = new Image();
-windyImage.src = "Resources/windy.png";
-
-const rainyImage = new Image();
-rainyImage.src = "Resources/rainy.png";
-
-const snowyImage = new Image();
-snowyImage.src = "Resources/snowy.png";
-
-const imageArray = [sunnyImage.src, cloudyImage.src, windyImage.src, rainyImage.src, snowyImage.src];
-
-const firstImage = document.getElementById("image1");
-const secondImage = document.getElementById("image2");
-const thirdImage = document.getElementById("image3");
-const fourthImage = document.getElementById("image4");
-
-const gridImages = {
-    currentDay: firstImage,
-    secondDay: secondImage,
-    thirdDay: thirdImage,
-    fourthDay: fourthImage
+// API requires LAT and LON coordinates. This function will obtain that data from the entered zip code and return it for future use
+async function getZipLat(zipCode){
+    const apiZipUrl = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${apiKey}`;
+    const response = await fetch(apiZipUrl);
+    // console.log(response);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    const lat = jsonResponse.lat;
+    console.log(lat);
+    return lat;
 };
 
-const weatherData = {
-    location: "Keller, TX",
-    currentWeather: "windy",
-    day2Weather: "cloudy",
-    day3Weather: "rainy",
-    day4Weather: "sunny"
+async function getZipLon(zipCode){
+    const apiZipUrl = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${apiKey}`;
+    const response = await fetch(apiZipUrl);
+    // console.log(response);
+    const jsonResponse = await response.json();
+    const lon = jsonResponse.lon;
+    console.log(lon);
+    return lon;
 };
+
+function getCityData() {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${getZipLat}&lon=${getZipLon}&appid=${apiKey}`;
+    console.log(apiUrl);
+};
+
+
+
+
+
+
+// const sunnyImage = new Image();
+// sunnyImage.src = "Resources/sunny.png";
+
+// const cloudyImage = new Image();
+// cloudyImage.src = "Resources/cloudy.png";
+
+// const windyImage = new Image();
+// windyImage.src = "Resources/windy.png";
+
+// const rainyImage = new Image();
+// rainyImage.src = "Resources/rainy.png";
+
+// const snowyImage = new Image();
+// snowyImage.src = "Resources/snowy.png";
+
+// const imageArray = [sunnyImage.src, cloudyImage.src, windyImage.src, rainyImage.src, snowyImage.src];
+
+// const firstImage = document.getElementById("image1");
+// const secondImage = document.getElementById("image2");
+// const thirdImage = document.getElementById("image3");
+// const fourthImage = document.getElementById("image4");
+
+// const gridImages = {
+//     currentDay: firstImage,
+//     secondDay: secondImage,
+//     thirdDay: thirdImage,
+//     fourthDay: fourthImage
+// };
+
+// const weatherData = {
+//     location: "Keller, TX",
+//     currentWeather: "windy",
+//     day2Weather: "cloudy",
+//     day3Weather: "rainy",
+//     day4Weather: "sunny"
+// };
