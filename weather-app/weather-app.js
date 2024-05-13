9effdf3195b020f134fef8b4b1da0fda3e0e58ef
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-05-12 22:20:56 -0500

    completed functionality for getting weather data for days 2-4. next will be to add event listener for hitting enter on the keystroke to get the same result. also need to clean up the css before being finished with app

--- weather-app/weather-app.js
+++ weather-app/weather-app.js
@@ -7,6 +7,7 @@ const weatherBox = document.querySelector(".weatherDisplayBox");
 const inputBox = document.getElementById("locationBox");
 const searchButton = document.getElementById("searchButton");
 const apiKey = "bebce22beee5345111cd44dd8326a9db";
+const apiKey2 = "047cf777409b3bed3802d38b91648d3a";
 
 const cloudyGif = "Resources/cloudy-gif.gif";
 const sunnyGif = "Resources/sunny-gif.gif";
@@ -67,7 +68,7 @@ async function getCityData(zipCode) {
     const lat = responseLatLon.lat;
     const lon = responseLatLon.lon;
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     const response2 = await fetch(apiUrl);
     const cityResponse = await response2.json();
     console.log(cityResponse);
@@ -75,9 +76,25 @@ async function getCityData(zipCode) {
     return cityResponse;
 }; 
 
+async function getCityData2(zipcode2) { 
+    const apiZipUrl = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode2}&appid=${apiKey2}`;
+    const response1 = await fetch(apiZipUrl);
+    const responseLatLon = await response1.json();
+    console.log(responseLatLon)
+    const lat = responseLatLon.lat;
+    const lon = responseLatLon.lon;
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=4&appid=${apiKey2}&units=imperial`;
+    const response2 = await fetch(apiUrl);
+    const cityResponse = await response2.json();
+    console.log(cityResponse);
+
+    return cityResponse;
+}
+
 // Need to pull weather data from json format and return needed information to update current and future weather
 
-async function getWeatherData() {
+async function getCurrentWeatherData() {
     const location = locationInput.value;
     const data = await getCityData(location);
     const cityName = data.name
@@ -86,9 +103,7 @@ async function getWeatherData() {
     displayCityName.textContent = cityName;
     console.log(cityName);
 
-    let kelvinTemp = data.main.temp;
-    let celsiusTemp = kelvinTemp - 273;
-    let fahrenheit = celsiusTemp * (9 / 5) + 32;
+    let fahrenheit = data.main.temp
     fahrenheit = Math.floor(fahrenheit);
     const displayCurrentTemp = document.getElementById("currentTemp");
     displayCurrentTemp.textContent = fahrenheit;
@@ -129,8 +144,89 @@ async function getWeatherData() {
     console.log(weatherId);
 }
 
+// Create a function to gather the data for days 2, 3 and 4 and display them on the DOM
+
+async function getOtherDayWeatherData() {
+    const location2 = locationInput.value;
+    const data2 = await getCityData2(location2);
+
+    let day2Fahrenheit = data2.list[1].main.temp;
+    day2Fahrenheit = Math.floor(day2Fahrenheit);
+
+    let day3Fahrenheit = data2.list[2].main.temp;
+    day3Fahrenheit = Math.floor(day3Fahrenheit);
+
+    let day4Fahrenheit = data2.list[3].main.temp;
+    day4Fahrenheit = Math.floor(day4Fahrenheit);
+
+    const displayDay2Temp = document.getElementById("secondTemp");
+    displayDay2Temp.textContent = day2Fahrenheit;
+
+    const displayDay3Temp = document.getElementById("thirdTemp");
+    displayDay3Temp.textContent = day3Fahrenheit;
+
+    const displayDay4Temp = document.getElementById("fourthTemp");
+    displayDay4Temp.textContent = day4Fahrenheit;
+
+    console.log(day2Fahrenheit, day3Fahrenheit, day4Fahrenheit);
+
+    let weatherImageSrc2 = "";
+    let weatherImageSrc3 = "";
+    let weatherImageSrc4 = "";
+    const weatherId2 = data2.list[1].weather[0].id;
+    const weatherId3 = data2.list[2].weather[0].id;
+    const weatherId4 = data2.list[3].weather[0].id;
+    
+    if (weatherIdNumbers.rainy.includes(weatherId2)) {
+        weatherImageSrc2 = weatherImg.Rainy;
+    } else if (weatherIdNumbers.snowy.includes(weatherId2)) {
+        weatherImageSrc2 = weatherImg.Snowy;
+    } else if (weatherIdNumbers.cloudy.includes(weatherId2)) {
+        weatherImageSrc2 = weatherImg.Cloudy;
+    } else if (weatherIdNumbers.clear.includes(weatherId2)) {
+        weatherImageSrc2 = weatherImg.Sunny;
+    } else {
+        weatherImageSrc2 = weatherImg.Other;
+    }
+
+    const currentWeatherImg2 = document.getElementById("image2");
+    currentWeatherImg2.src = weatherImageSrc2;
+
+    if (weatherIdNumbers.rainy.includes(weatherId3)) {
+        weatherImageSrc3 = weatherImg.Rainy;
+    } else if (weatherIdNumbers.snowy.includes(weatherId3)) {
+        weatherImageSrc3 = weatherImg.Snowy;
+    } else if (weatherIdNumbers.cloudy.includes(weatherId3)) {
+        weatherImageSrc3 = weatherImg.Cloudy;
+    } else if (weatherIdNumbers.clear.includes(weatherId3)) {
+        weatherImageSrc3 = weatherImg.Sunny;
+    } else {
+        weatherImageSrc3 = weatherImg.Other;
+    }
+
+    const currentWeatherImg3 = document.getElementById("image3");
+    currentWeatherImg3.src = weatherImageSrc3;
+
+    if (weatherIdNumbers.rainy.includes(weatherId4)) {
+        weatherImageSrc4 = weatherImg.Rainy;
+    } else if (weatherIdNumbers.snowy.includes(weatherId4)) {
+        weatherImageSrc4 = weatherImg.Snowy;
+    } else if (weatherIdNumbers.cloudy.includes(weatherId4)) {
+        weatherImageSrc4 = weatherImg.Cloudy;
+    } else if (weatherIdNumbers.clear.includes(weatherId4)) {
+        weatherImageSrc4 = weatherImg.Sunny;
+    } else {
+        weatherImageSrc4 = weatherImg.Other;
+    }
+
+    const currentWeatherImg4 = document.getElementById("image4");
+    currentWeatherImg4.src = weatherImageSrc4;
+
+}
+
 // Create a main function that runs everything based on the event listener for clicking the submit button
 
 searchButton.addEventListener("click", event => {
-    getWeatherData();
+    getCurrentWeatherData();
+    getOtherDayWeatherData();
 });
